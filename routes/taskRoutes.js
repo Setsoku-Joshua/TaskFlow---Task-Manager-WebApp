03aa353a0bf3b47675859ede8458e63d20f61029
Setsoku <jorod191@gmail.com>
2026-05-30 17:33:07 +0800

    initial file upload (backend)

---
+++ routes/taskRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    createTask,
+    getTasks,
+    getTask,
+    updateTask,
+    deleteTask
+} = require('../controllers/taskController');
+
+const { protect } = require('../middleware/authMiddleware');
+
+
+router.route('/')
+    .get(protect, getTasks)
+    .post(protect, createTask);
+
+router.route('/:id')
+    .get(protect, getTask)
+    .put(protect, updateTask)
+    .delete(protect, deleteTask);
+
+module.exports = router;
